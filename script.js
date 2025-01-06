4b4fa994242fb24d75511884026181f69f717222
Nobodip Debnath <nobodipdebnath45@.com>
2025-01-07 00:27:18 +0600

    add all file

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link")
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
